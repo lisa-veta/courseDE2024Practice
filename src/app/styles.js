7e85fa82a0ffc4c36b75c0e987c491b893a5dbc1
Elizaveta Pernikova <lisapernikowa@mail.ru>
2024-11-07 15:27:25 +0500

    fix: styles, ChoiceElem

--- src/app/styles.js
+++ src/app/styles.js
@@ -1,7 +1,33 @@
+import "choices.js/public/assets/styles/choices.css";
 const files = await import.meta.webpackContext("../", {
-    regExp: /\.pcss/,
-    mode: "eager",
+  regExp: /\.pcss/,
+  mode: "eager",
 });
 
-const styles = await Promise.all(files.keys().map((path) => files(path)));
+// Определите массив с приоритетами
+const priorityOrder = [
+  "./app/styles/vars.pcss",
+  "./app/styles/palette.pcss",
+  "./app/styles/fonts.pcss",
+  "./app/styles/global.pcss",
+  "./app/styles/utils.pcss",
+];
+
+// Получите все файлы
+const allFiles = files.keys();
+
+// Фильтруйте файлы, чтобы разделить на те, которые в приоритете, и остальные
+const prioritizedStyles = priorityOrder.filter(
+  (name) => allFiles.includes(name) // Проверяем на соответствие полному пути
+);
+
+// Отфильтруйте остальные стили
+const otherStyles = allFiles.filter(
+  (path) => !prioritizedStyles.includes(path)
+);
+
+// Объедините массивы, чтобы сначала загружались приоритетные стили
+const orderedFiles = [...prioritizedStyles, ...otherStyles];
+
+const styles = await Promise.all(orderedFiles.map((path) => files(path)));
 export { styles };
--- src/shared/ui/ChoiceElem/model/index.js
+++ src/shared/ui/ChoiceElem/model/index.js
@@ -9,21 +9,176 @@ export class ChoiceElemModel {
     instance: "[data-js-custom-select]",
   };
 
+  static instances = [];
+
+  static presets = {
+    default: {
+      createItem: (
+        { classNames },
+        data,
+        { strToEl, escapeForTemplate, getClassNames }
+      ) => {
+        return strToEl(`
+          <div class="${getClassNames(classNames.item).join(" ")} 
+          ${getClassNames(
+            data.highlighted
+              ? classNames.highlightedState
+              : classNames.itemSelectable
+          ).join(" ")} 
+          ${data.placeholder ? classNames.placeholder : ""}" 
+          data-item 
+          data-id="${data.id}" 
+          data-value="${data.value}" 
+          ${data.active ? 'aria-selected="true"' : ""}
+          ${data.disabled ? 'aria-disabled="true"' : ""}>
+            <span class="customSelect__choiceIcon">${data?.customProperties?.icon ?? ""}</span><span>${data.label}</span>
+          </div>
+        `);
+      },
+      createChoice: (
+        { classNames },
+        data,
+        { strToEl, escapeForTemplate, getClassNames },
+        itemSelectText
+      ) => {
+        console.debug(data, "!!!");
+        return strToEl(`
+          <div class="${getClassNames(classNames.item).join(" ")} 
+            ${getClassNames(classNames.itemChoice).join(" ")} 
+            ${getClassNames(
+              data.disabled
+                ? classNames.itemDisabled
+                : classNames.itemSelectable
+            ).join(" ")}" 
+            data-select-text="${itemSelectText}" 
+            data-choice 
+            ${data.disabled ? 'data-choice-disabled aria-disabled="true"' : "data-choice-selectable"}
+            data-id="${data.id}" 
+            data-value="${data.value}">
+            <span class="customSelect__choiceIcon">${data?.customProperties?.icon ?? ""}</span><span>${data.label}</span>
+          </div>
+        `);
+      },
+    },
+    fancy: {
+      createItem: (
+        { classNames },
+        data,
+        { strToEl, escapeForTemplate, getClassNames }
+      ) => {
+        return strToEl(`
+          <div class="${getClassNames(classNames.item).join(" ")} 
+          ${getClassNames(data.highlighted ? classNames.highlightedState : classNames.itemSelectable).join(" ")} 
+          ${data.placeholder ? classNames.placeholder : ""}" 
+          data-item 
+          data-id="${data.id}" 
+          data-value="${data.value}"
+          ${data.active ? 'aria-selected="true"' : ""}
+          ${data.disabled ? 'aria-disabled="true"' : ""}>
+            🌟 ${data.label}
+          </div>
+        `);
+      },
+      createChoice: (
+        { classNames },
+        data,
+        { strToEl, escapeForTemplate, getClassNames },
+        itemSelectText
+      ) => {
+        return strToEl(`
+          <div class="${getClassNames(classNames.item).join(" ")} 
+          ${getClassNames(classNames.itemChoice).join(" ")} 
+          ${getClassNames(data.disabled ? classNames.itemDisabled : classNames.itemSelectable).join(" ")}"
+          data-choice 
+          data-select-text="${itemSelectText}" 
+          ${data.disabled ? 'data-choice-disabled aria-disabled="true"' : "data-choice-selectable"}
+          data-id="${data.id}" 
+          data-value="${data.value}">
+            🌟 ${data.label}
+          </div>
+        `);
+      },
+    },
+  };
+
   static defaultCfg = {
     itemSelectText: "",
+    classNames: {
+      containerOuter: ["choices", "customSelect"],
+      itemChoice: ["choices__item--choice", "customSelect__choice"],
+      containerInner: ["choices__inner", "customSelect__inner"],
+      list: ["choices__list", "customSelect__list"],
+      itemSelectable: [
+        "choices__item--selectable",
+        "customSelect__item--selectable",
+      ],
+    },
+    callbackOnCreateTemplates: function (
+      strToEl,
+      escapeForTemplate,
+      getClassNames,
+      { preset, itemSelectText }
+    ) {
+      const presetName = preset || "default";
+      const { createItem, createChoice } =
+        ChoiceElemModel.presets[presetName] || ChoiceElemModel.presets.default;
+      return {
+        item: (classNames, data) =>
+          createItem(classNames, data, {
+            strToEl,
+            escapeForTemplate,
+            getClassNames,
+          }),
+        choice: (classNames, data) =>
+          createChoice(
+            classNames,
+            data,
+            {
+              strToEl,
+              escapeForTemplate,
+              getClassNames,
+            },
+            itemSelectText
+          ),
+      };
+    },
   };
+
+  static createCustomSelect(node) {
+    const cfg = getCfg(node, "data-js-custom-select");
+    const { disableTemplates, preset, ...restCfg } = cfg;
+    const choicesConfig = {
+      ...ChoiceElemModel.defaultCfg,
+      ...restCfg,
+    };
+    if (disableTemplates) {
+      delete choicesConfig.callbackOnCreateTemplates;
+    }
+    choicesConfig.callbackOnCreateTemplates = ((originalCallback) => {
+      return function (...args) {
+        return originalCallback.apply(this, [
+          ...args,
+          { preset, itemSelectText: choicesConfig.itemSelectText },
+        ]);
+      };
+    })(choicesConfig.callbackOnCreateTemplates);
+    ChoiceElemModel.instances.push(
+      new Choices(node, {
+        ...choicesConfig,
+      })
+    );
+  }
+
+  static getInstance(node) {
+    return ChoiceElemModel.instances.find(
+      (instance) => instance.passedElement.element === node
+    );
+  }
 
   constructor() {
     this.selects = document.querySelectorAll(this.selectors.instance);
-    this.instances = [];
     this.selects.forEach((select) => {
-      const cfg = getCfg(select, "data-js-custom-select");
-      this.instances.push(
-        new Choices(select, {
-          ...ChoiceElemModel.defaultCfg,
-          ...cfg,
-        })
-      );
+      ChoiceElemModel.createCustomSelect(select);
     });
   }
 }
