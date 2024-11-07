b0fe6d8772676550c825ee7b9e6062e145f68ac6
Elizaveta Pernikova <lisapernikowa@mail.ru>
2024-11-07 16:41:01 +0500

    feat: add MockData

---
+++ src/shared/api/MockData.js
@@ -0,0 +1,67 @@
+export const mockData = {
+  markerDetail: {
+    isSuccess: true,
+    data: {
+      id: "12",
+      title: "Al Capone",
+      type: "1",
+      address: {
+        city: "Челябинск",
+        house: "12a",
+        street: "ул. Братьев Кашириных",
+      },
+      comment:
+        "Хороший бар и караоке, по средам у них специальные акции с коктейлями",
+      images: [
+        "/images/image1.png",
+        "/images/image2.png",
+        "/images/image3.png",
+        "/images/image4.png",
+      ],
+    },
+  },
+  markersList: {
+    markers: [
+      {
+        id: "12",
+        type: "1",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "13",
+        type: "2",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "14",
+        type: "3",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "15",
+        type: "4",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "16",
+        type: "5",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "17",
+        type: "5",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "18",
+        type: "5",
+        cords: [56.5, 57.9],
+      },
+      {
+        id: "19",
+        type: "5",
+        cords: [56.5, 57.9],
+      },
+    ],
+  }
+};
--- src/shared/api/index.js
+++ src/shared/api/index.js
@@ -1,53 +1,16 @@
 import { getResponseMock } from "./lib/index.js";
 import { API_ENDPOINTS } from "../config/constants.js";
+import { mockData } from "./mockData.js"
 
 export const handlers = [
   getResponseMock({
     type: "GET",
     endpoint: API_ENDPOINTS.markers.detail,
-    data: {
-      markers: [
-        {
-          id: "12",
-          type: "1",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "13",
-          type: "2",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "14",
-          type: "3",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "15",
-          type: "4",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "16",
-          type: "5",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "17",
-          type: "5",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "18",
-          type: "5",
-          cords: [56.5, 57.9],
-        },
-        {
-          id: "19",
-          type: "5",
-          cords: [56.5, 57.9],
-        },
-      ],
-    },
+    data: mockData.markersList,
+  }),
+  getResponseMock({
+    type: "GET",
+    endpoint: API_ENDPOINTS.markers.detail,
+    data: mockData.markerDetail,
   }),
 ];
